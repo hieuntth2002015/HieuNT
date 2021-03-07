65460111801c201c525ec306a689218e2430372e
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-03-07 21:51:03 +0700

    task11

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+         <title>TodoList- Nguyen Trung Hieu</title>
+     </head>
+     <body>
+         <div class="container">
+             <nav class="navbar navbar-default">
+                 Navbar Contents
+             </nav>
+         </div>
+         @yield('content')
+     </body>
+</html>
